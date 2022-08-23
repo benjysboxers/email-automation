2c7edebf2c3b0f1eb5844a6061de7ec5d21afbfb
Philip <94329833+benjysboxers@users.noreply.github.com>
2022-08-22 19:38:38 -0600

    Update automate_email.py

--- automate_email.py
+++ automate_email.py
@@ -2,20 +2,23 @@ import smtplib #SMTP client session object to send mail to any internet mahine w
 import ssl #encryption and peer authentication facilities for network sockets, both client and server side
 from email.message import EmailMessage
 
-email_sender =''
-email_pass = ''
-email_receiver = ''
+email_sender ='' # the gmail sending the mail
+email_pass = '' # 16 character password you generated goes in here
+email_receiver = '' # the person recieving the gmail (you can't test it with an alt gmail or your friends if you want)
 
 subject = 'Testing sending emails using python'
 body = """
 Trying to do something with my braincells.exe
 please don't laugh about it.
-"""
+"""                                         # content you wish to send across goes in here 
+
 em = EmailMessage()
-em['From'] = email_sender
+
+#passing our assigned variables from before into a formated structure 
+em['From'] = email_sender 
 em['To'] = email_receiver
 em['Subject'] = subject
-em.set_content(body)
+em.set_content(body) #basically a structured format as seen in the gmail when you usually compose something to send 
 
 context = ssl.create_default_context() # used for security and safeguarding any sensitive data that is sent between two systems
 
